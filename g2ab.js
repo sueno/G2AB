f8fde96cfe899830e07ed1852027fdfb09ef4a08
sueno <ueno.ntnu@gmail.com>
2013-01-31 23:06:43 +0900

    first commit

---
+++ g2ab.js
@@ -0,0 +1,25 @@
+$(window).load(function(){
+		  $('div' + '.amebloGist').each(function(){
+		    var param = $(this).attr('id');
+		    if(param && param.match(/^[0-9]*$/)){
+            	var giturl = 'https://gist.github.com/'+param+'.json'
+	            $.ajax({
+	                url: giturl,
+	                type: 'GET',
+	                dataType: 'jsonp'
+	              }).success(function(gistdata) {
+                     var gistlink = document.createElement("link");
+	            	  gistlink.href = gistdata.stylesheet
+	            	  gistlink.media = "screen"
+	            	  gistlink.rel = "stylesheet"
+	                 document.head.appendChild(gistlink);
+          	 		 $('#'+param).html(gistdata.div)
+          	 		 
+	              }).error(function() {
+	            	 alert("error") 
+	              });
+		    }else{
+		      notice(param, "Illigal parameter.");
+		    }
+		  });
+		});
